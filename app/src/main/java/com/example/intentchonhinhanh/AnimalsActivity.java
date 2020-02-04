65344415c1638318e656af9f64c27da703b6c4d6
Dinh Quang <quangddinh@gmail.com>
2020-02-05 02:09:56 +0700

    finish intent activity

---
+++ app/src/main/java/com/example/intentchonhinhanh/AnimalsActivity.java
@@ -0,0 +1,14 @@
+package com.example.intentchonhinhanh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimalsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animals);
+    }
+}
--- app/src/main/java/com/example/intentchonhinhanh/MainActivity.java
+++ app/src/main/java/com/example/intentchonhinhanh/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.intentchonhinhanh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView mImgHinhGoc,mImgHinhChon;
+    TextView mTvDiemSo;
+    String[] arrayNamesAnimal;
+    int mIndexHinhGoc = -1;
+    int mValueHinhGoc = -1;
+    int mRequestCodeImage = 123;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        init();
+        mapview();
+        mapImageRandom();
+        setListener();
+
     }
+
+    private void setListener() {
+        mImgHinhChon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this,AnimalsActivity.class);
+//                startActivityForResult(intent,mRequestCodeImage);
+                Intent intent = new Intent(MainActivity.this,AnimalsActivity.class);
+                startActivityForResult(intent,mRequestCodeImage);
+            }
+        });
+    }
+
+    private void init() {
+        arrayNamesAnimal = getResources().getStringArray(R.array.arrayNamesAnimal);
+
+    }
+    private void mapview() {
+        mImgHinhChon = findViewById(R.id.imageviewHinhchon);
+        mImgHinhGoc = findViewById(R.id.imageviewHinhgoc);
+        mTvDiemSo = findViewById(R.id.textviewdiem);
+    }
+    private void mapImageRandom() {
+        mIndexHinhGoc = new Random().nextInt(arrayNamesAnimal.length);
+        mValueHinhGoc = getResources().getIdentifier(
+                arrayNamesAnimal[mIndexHinhGoc],
+                "drawable",
+                getPackageName());
+        mImgHinhGoc.setImageResource(mValueHinhGoc);
+    }
+
 }
