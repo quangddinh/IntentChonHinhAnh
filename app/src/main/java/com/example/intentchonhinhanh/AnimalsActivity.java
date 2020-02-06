088bb1d0bd82a2547f069d9631dc06c87dd331e4
Dinh Quang <quangddinh@gmail.com>
2020-02-06 19:34:32 +0700

    id image

--- app/src/main/java/com/example/intentchonhinhanh/AnimalsActivity.java
+++ app/src/main/java/com/example/intentchonhinhanh/AnimalsActivity.java
@@ -4,32 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 public class AnimalsActivity extends AppCompatActivity {
+
     TableLayout mTablelayout;
+    String[] arrayNamesAnimal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals);
 
+        init();
         mapview();
         drawimage();
+
+
+    }
+
+    private void init() {
+        arrayNamesAnimal = getResources().getStringArray(R.array.arrayNamesAnimal);
+        // cho mang ten random
+        Collections.shuffle(Arrays.asList(arrayNamesAnimal));
 
     }
 
     private void drawimage() {
         int socot = 3;
         int sodong = 6;
+        int index = 0;
         for (int i = 0; i < sodong ; i++)
         {
             Context context;
             TableRow tableRow = new TableRow(AnimalsActivity.this);
             for (int j = 0; j < socot ; j++){
-                ImageView imageView = new ImageView(AnimalsActivity.this);
-                imageView.setImageResource(R.drawable.bo);
+                final ImageView imageView = new ImageView(AnimalsActivity.this);
+
+                int valueHinhChon = getResources().getIdentifier(
+                        arrayNamesAnimal[index++],
+                        "drawable",
+                        getPackageName());
+
+                imageView.setImageResource(valueHinhChon);
+
+                imageView.setTag(valueHinhChon);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(AnimalsActivity.this, imageView.getTag().toString() + "", Toast.LENGTH_SHORT).show();
+                        // du lieu id hinh`
+                    }
+                });
                 tableRow.addView(imageView);
             }
             mTablelayout.addView(tableRow);
